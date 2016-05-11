5c97817133bf0709763e5976d71b0e407f7463db
Jake Schroeder <Trace@jakes-macbook-pro.local>
2016-05-10 21:01:47 -0500

    Initial Commit

---
+++ AzureADTest/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  AzureADTest
+//
+//  Created by Jake Schroeder on 5/10/16.
+//  Copyright © 2016 Incline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ AzureADTest/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  AzureADTest
+//
+//  Created by Jake Schroeder on 5/10/16.
+//  Copyright © 2016 Incline. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
